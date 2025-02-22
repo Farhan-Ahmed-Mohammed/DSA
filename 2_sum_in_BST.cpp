cdeaf1f9d60ff26428cc6b43f6aef980f66791cd
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-02-22 06:58:21 +0530

    Create 2_sum_in_BST.cpp

---
+++ 2_sum_in_BST.cpp
@@ -0,0 +1,54 @@
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+    
+};
+***************/
+vector<int> ans;
+void inorder(BinaryTreeNode<int>* root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        ans.push_back(root->data);
+        inorder(root->right);
+    }
+
+    
+   
+}
+
+bool twoSumInBST(BinaryTreeNode<int>* root, int target) {
+	  ans.clear(); 
+     inorder(root);
+     int left=0;
+     int right=ans.size()-1;
+
+     while(left<right)
+     {
+         int sum=ans[left]+ans[right];
+         if(sum==target)
+         {
+             return true;
+         }
+
+         else if(sum<target)
+         {
+             left++;
+         }
+
+         else{
+             right--;
+         }
+     }
+     return false;
+    //Write your code here
+}
