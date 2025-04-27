5efad1e652206724c3ed562d5098920c09f52649
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-04-27 15:30:34 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -9,7 +9,7 @@ int main()
     for(int i=0;i<n;i++)
     {   int temp=arr[i];//store in temp as i value changes
         int j=i-1;
-        for(;j>=0;j--)
+        for(;j>=0;j--) //when i=0 if wont enter this loop as j=-1 
         {
          if(arr[j]>temp)
             {
@@ -19,8 +19,8 @@ int main()
             {
                 break;
             }
-        }
-    arr[j+1]=temp;
+        }  
+    arr[j+1]=temp;  //j bcms -1 it come out of loop and now j+1 means j=0 or when if loop fails also it come out
 }
 for(int i=0;i<n;i++)
 {
