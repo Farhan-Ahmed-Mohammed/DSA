a3d34bbaf3b05ce539d2c720af731ee90795a03e
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-10-04 11:35:16 +0530

    Update dp_knap_sack.cpp

--- dp_knap_sack.cpp
+++ dp_knap_sack.cpp
@@ -32,6 +32,32 @@ int solveMem(vector<int> &weight,vector<int> &value,int index,int capacity,vecto
 		return dp[index][capacity];
 	
 } 
+
+int solve(vector<int> &wt,vector<int> &val,int n,int capacity)  // this is botto up approach tabulation
+{
+	vector<vector<int>> dp(n+1,vector<int>(capacity+1,0));
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int w=0;w<=capacity;w++)
+		{
+			if(w>=wt[i-1])
+			{			
+				dp[i][w]=max(dp[i-1][w],val[i-1]+dp[i-1][w-wt[i-1]]);
+			}
+
+			else
+			{
+				dp[i][w]=dp[i-1][w];
+			}
+		}
+	}
+	return dp[n][capacity];
+}
+
+
+
+	
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
 {
 	vector<vector<int>> dp(n,vector<int> (maxWeight+1,-1));
